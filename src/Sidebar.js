d14638a8e86950cb48f3c10ffb1761134a112e11
Megan McKeough <megan.mckeough@gmail.com>
2018-09-06 21:23:17 +1000

    sidebar complete and styled

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -8,7 +8,7 @@ export default class Sidebar extends React.Component {
 				<nav><a onClick={ this.props.hidebar } href="#">>></a></nav>
 				<p className="about">This site was built by <a href="https://www.linkedin.com/in/megan-mckeough/">me</a>, Megan McKeough, with React.js, <a href="http://open-notify.org/">Open Notify</a>, <a href="http://www.geonames.org/">GeoNames</a>, and <a href="https://countryflags.io/">CountryFlags.io</a> APIs, and a love of space. Visit the <a href="https://github.com/meganmckeough/iss">Github</a> for more.
 				</p>
-				<p>Big thanks to <a href="https://generalassemb.ly/education/web-development-immersive">General Assembly</a>; and WDI16 represent.</p> 
+				<p>Big thanks to <a href="https://generalassemb.ly/education/web-development-immersive">General Assembly</a>; and long live WDI16.</p> 
 				<p>2018</p>
 				<img className="cute" src="../cute.gif_c200" alt=""/>
 			</div>
