5a8c11570cefc446e0e8929edba33e7949d75896
Megan McKeough <megan.mckeough@gmail.com>
2018-09-06 21:51:06 +1000

    minor sidebar tweaks and readme

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -5,10 +5,13 @@ export default class Sidebar extends React.Component {
 	render() {
 		return (
 			<div className={"sidebar " + this.props.className}>
-				<nav><a onClick={ this.props.hidebar } href="#">>></a></nav>
-				<p className="about">This site was built by <a href="https://www.linkedin.com/in/megan-mckeough/">me</a>, Megan McKeough, with React.js, <a href="http://open-notify.org/">Open Notify</a>, <a href="http://www.geonames.org/">GeoNames</a>, and <a href="https://countryflags.io/">CountryFlags.io</a> APIs, and a love of space. Visit the <a href="https://github.com/meganmckeough/iss">Github</a> for more.
-				</p>
+				<nav><a onClick={ this.props.hidebar } href="#">Hide</a></nav>
+				<p className="about">This site was built by <a href="https://www.linkedin.com/in/megan-mckeough/">me</a>, Megan McKeough, with React.js, <a href="http://open-notify.org/">Open Notify</a>, <a href="http://www.geonames.org/">GeoNames</a>, and <a href="https://countryflags.io/">CountryFlags.io</a> APIs, and a love of space (the final frontier).</p>
+
+				<p>Visit the <a href="https://github.com/meganmckeough/iss">Github</a> for more.</p>
+
 				<p>Big thanks to <a href="https://generalassemb.ly/education/web-development-immersive">General Assembly</a>; and long live WDI16.</p> 
+				
 				<p>2018</p>
 				<img className="cute" src="../cute.gif_c200" alt=""/>
 			</div>
