ee6affe1de8bea52b5951aa3255fe0c43665cf45
Megan McKeough <megan.mckeough@gmail.com>
2018-09-06 15:30:01 +1000

    added animated earth

--- src/App.js
+++ src/App.js
@@ -5,41 +5,46 @@ import './People.css'
 import './Location.css'
 import './NextPass.css'
 import './Star.css'
+import './Distance.css'
 import axios from 'axios'
 
 import People from './People'
 import Location from './Location'
 import NextPass from './NextPass'
+import Distance from './Distance'
 
 class App extends Component {
 
 	state = {}
-
-  	render() {
+		
+	render() {
 	    return (
 	   
-	      <div className="App">
+	      	<div className="App">
 			
-			<div className="stars"></div>
-			<div className="twinkling"></div>
-			<div className="p p-1"></div>
-			<div className="p p-2"></div>
+				<div className="stars"></div>
+				<div className="twinkling"></div>
+				<div className="p p-1"></div>
+				<div className="p p-2"></div>
 
 
-			<nav>About</nav>
-	        <h1>Where [over] the world is the ISS?</h1>
-	        <p className="description">The International Space Station (ISS) orbits the Earth at the rate of 7.66km per second, around once every 92 minutes.</p>
-	        <img id="earth" src="../earth2.gif" alt=""/>
+				<nav>About</nav>
+		        <h1>Where [over] the world is the ISS?</h1>
+		        <p className="description">The International Space Station (ISS) orbits the Earth at the rate of 7.66km per second, around once every 92 minutes.</p>
+		        <Distance />
 
-			<Location />
-			<NextPass />
-	        <People />
+				<Location />
+				<NextPass />
+		        <People />
+		        <img id="earth" src="../earth2.gif" alt=""/>
 
-			<footer><a href="https://www.linkedin.com/in/megan-mckeough/" target="_blank">Megan McKeough 2018</a></footer>
+				<footer>
+					<a href="https://www.linkedin.com/in/megan-mckeough/" target="_blank">Megan McKeough 2018</a>
+				</footer>
 			
-	      </div>
+	      	</div>
 	    )
-  	}
+	}
 }
 
 export default App;
--- src/Distance.js
+++ src/Distance.js
@@ -1,16 +1,20 @@
 import React from 'react'
 
 export default class Location extends React.Component {
-	
-	state = {}
-
-	componentWillMount = () => {
-		
-	}
 
 	render() {
 
-		return ()
+		return (
+			<div className="container">
+				<div className="satellite-move">
+				<img id="satellite" class="satellite-rotate" src="../satellite2.png" alt=""/>
+				</div>
+				<img id="earth" src="../worldwide.png" alt=""/>
+			</div>
+		)
 
 	}
 }
+
+
+
